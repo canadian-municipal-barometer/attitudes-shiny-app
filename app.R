c82011fe2edb4a031f330ba81cdc1314727cda38
Reed <reedjmerrill@gmail.com>
2025-02-27 20:19:13 -0700

    partial prototype

--- app.R
+++ app.R
@@ -1,14 +1,31 @@
 library(shiny)
 
-# Define UI for app that draws a histogram ----
 ui <- fluidPage(
-
-  # App title ----
-  titlePanel("Canadian's Policy Attitudes")
+  sidebar = sidebar(
+    selectInput(
+      inputId = "policy-selector",
+      label = "Policy",
+      choices = c(
+        "Subsidized Housing" = "all_loc_1",
+        "Public Transit" = "all_loc_3"
+      )
+    )
+  ),
+  titlePanel("Canadian's Policy Attitudes"),
+  textOutput("selected_var")
 )
 
 # Define server logic required to draw a histogram ----
-server <- function(input, output) {}
+server <- function(input, output) {
+  con <- dbConnect(duckdb::duckdb(), dbdir = ":memory:")
+  initialize_database(con, "data/policy-data.duckdb", table = "policy-data")
+
+  df <- dplyr::tbl(con, "policy-data")
+
+  output$selected_var <- renderText({
+
+  })
+}
 
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
