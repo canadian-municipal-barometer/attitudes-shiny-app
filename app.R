fe4fef4b6ea8fbe26fca1d4ff266e0cabdc44d23
Reed <reedjmerrill@gmail.com>
2025-03-06 15:34:01 -0700

    explicit namespacing

--- app.R
+++ app.R
@@ -9,86 +9,92 @@ DBI::dbDisconnect(con)
 # load statement text list (`statement_lookup`)
 load("data/statement-text.rda")
 
-ui <- page_fillable(
-  titlePanel("Canadian's Policy Attitudes"),
-  selectInput(
+ui <- bslib::page_fillable(
+  shiny::titlePanel("Canadian's Policy Attitudes"),
+  shiny::selectInput(
     inputId = "policy",
     label = "Select a policy, or begin typing a subject of interest:",
     choices = statement_lookup$statement,
     selectize = TRUE,
-    width = "66vw"
+    width = "40vw"
   ),
-  layout_columns(
+  bslib::layout_columns(
     col_widths = c(3, 9),
-    card(
-      selectInput(
+    bslib::card(
+      shiny::selectInput(
         inputId = "gender",
         label = "Gender:",
         choices = levels(df$gender),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
         inputId = "agecat",
         label = "Age:",
         choices = levels(df$agecat),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
         inputId = "race",
         label = "Race:",
         choices = levels(df$race),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
         inputId = "education",
         label = "Education:",
         choices = levels(df$education),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
         inputId = "income",
         label = "Income:",
         choices = levels(df$income),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
         inputId = "immigrant",
         label = "Immigration Status:",
         choices = levels(df$immigrant),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
         inputId = "province",
         label = "Province:",
         choices = levels(df$province),
         selectize = FALSE
       ),
-      selectInput(
+      shiny::selectInput(
+        inputId = "popcat",
+        label = "Population:",
+        choices = levels(df$popcat),
+        selectize = FALSE
+      ),
+      shiny::selectInput(
         inputId = "homeowner",
         label = "Homeowner:",
         choices = levels(df$homeowner),
         selectize = FALSE
       ),
     ),
-    card(
-      textOutput("selected_var")
+    bslib::card(
+      shiny::textOutput("predictions")
     )
   )
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  output$selected_var <- renderText({
+  output$predictions <- shiny::renderText({
     # policy to filter the data by
     filter <- statement_lookup$var_name[statement_lookup$statement == input$policy]
 
     # translate the contents of the selectors to variable names
 
-    tmp_df <- glm()
+    tmp_df <- df |> dplyr::filter(policy == filter)
 
-    return(nrow(tmp_df))
+    return()
   })
 }
 
 # Create Shiny app ----
-shinyApp(ui = ui, server = server)
+shiny::shinyApp(ui = ui, server = server)
