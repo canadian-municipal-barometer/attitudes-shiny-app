224a184574eb3fac6e06deca56496804ab4c572f
Reed <reedjmerrill@gmail.com>
2025-03-27 13:11:20 -0600

    convert to observer-based language translation

--- app.R
+++ app.R
@@ -20,12 +20,8 @@ default_policies <- statements$statement[
 # Set the error that is displayed if model inputs aren't present for a policy
 input_err <- "The combination of the policy question and demographic characteristics that you have selected aren't in the data. Please make another selection." # nolint
 
-
-# Translation --------------------
 # load translation file
 i18n <- Translator$new(translation_csvs_path = "data/translation/")
-# Set initial language
-i18n$set_translation_language("en")
 
 ui <- fluidPage(
   # necessary for shiny.i18n reactive translation
@@ -133,7 +129,7 @@ ui <- fluidPage(
           radioButtons(
             inputId = "gender",
             label = "Gender:",
-            choices = list(
+            choices = c(
               "Woman",
               "Man"
             ),
@@ -153,7 +149,7 @@ ui <- fluidPage(
           radioButtons(
             inputId = "race",
             label = "Race:",
-            choices = list(
+            choices = c(
               "Racialized minority",
               "White"
             )
@@ -161,7 +157,7 @@ ui <- fluidPage(
           radioButtons(
             inputId = "immigrant",
             label = "Immigrant:",
-            choices = list(
+            choices = c(
               "Yes",
               "No"
             ),
@@ -170,7 +166,7 @@ ui <- fluidPage(
           radioButtons(
             inputId = "homeowner",
             label = "Homeowner:",
-            choices = list(
+            choices = c(
               "Yes",
               "No"
             ),
@@ -351,12 +347,11 @@ server <- function(input, output, session) {
   # Language toggle
 
   # make reactive i18n object
-  i18n_r <- reactive({
-    i18n
-  })
+  i18n_r <- reactiveValues(translator = i18n)
+
   # language toggle observer
   observeEvent(input$lang_toggle, {
-    # actionButton values start a 0 and go up by 1 every time they activate
+    # actionButton values start a 0 and go up by 1 every time it activate
     # So, all odd values of input$lang_toggle will occur when the app is in
     # English and thus the language should be updated to French.
     if (input$lang_toggle %% 2 == 1) {
@@ -364,18 +359,118 @@ server <- function(input, output, session) {
     } else {
       lang <- "en"
     }
-    shiny.i18n::update_lang(language = lang, session = session)
-    i18n_r()$set_translation_language(lang)
-    print(paste("Language change!", input$lang_toggle))
+    i18n_r$translator$set_translation_language(lang)
+
+    print(paste("Language change! Button val:", input$lang_toggle))
     print(paste("Current language:", lang))
-  })
-  # UI updates for menu item language
-  observe({
+
+    # UI updates for menu item language
+    updateSelectInput(session, "province",
+      label = i18n_r$translator$t("Province:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Alberta",
+          "British Columbia",
+          "Manitoba",
+          "New Brunswick",
+          "Newfoundland and Labrador",
+          "Nova Scotia",
+          "Ontario",
+          "Prince Edward Island",
+          "Quebec",
+          "Saskatchewan"
+        )
+      )
+    )
+
+    updateSelectInput(session, "popcat",
+      label = i18n_r$translator$t("Population:"),
+      choices = i18n_r$translator$t(
+        c(
+          "3000-9,999",
+          "10,000-49,999",
+          "50,000-249,999",
+          "250,000-999,999",
+          "1,000,000+"
+        )
+      )
+    )
+
     updateRadioButtons(session, "gender",
-      label = i18n_r()$t("Gender:"),
-      choices = c(
-        i18n_r()$t("Woman"),
-        i18n_r()$t("Man")
+      label = i18n_r$translator$t("Gender:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Man",
+          "Woman"
+        )
+      )
+    )
+
+    updateSelectInput(session, "agecat",
+      label = i18n_r$translator$t("Age:"),
+      choices = i18n_r$translator$t(
+        c(
+          "18-29",
+          "30-44",
+          "45-59",
+          "60+"
+        )
+      )
+    )
+
+    updateRadioButtons(session, "race",
+      label = i18n_r$translator$t("Race:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Racialized minority",
+          "White"
+        )
+      )
+    )
+
+    updateRadioButtons(session, "immigrant",
+      label = i18n_r$translator$t("Immigrant:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Yes",
+          "No"
+        )
+      )
+    )
+
+    updateRadioButtons(session, "homeowner",
+      label = i18n_r$translator$t("Homeowner:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Yes",
+          "No"
+        )
+      )
+    )
+
+    updateSelectInput(session, "education",
+      label = i18n_r$translator$t("Education:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Less than high school",
+          "High school",
+          "Associate's degree or trades",
+          "Bachelor's degree",
+          "Post-graduate degree"
+        )
+      )
+    )
+
+    updateSelectInput(session, "education",
+      label = i18n_r$translator$t("Education:"),
+      choices = i18n_r$translator$t(
+        c(
+          "Less than $49,999",
+          "$50,000 - $99,999",
+          "$100,000 - $149,999",
+          "$150,000 - $199,999",
+          "$200,000 or more"
+        )
       )
     )
   })
