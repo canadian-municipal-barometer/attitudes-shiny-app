1da4dd4fecda5b2f39d22a86384c878a3818f5a6
Reed <reedjmerrill@gmail.com>
2025-06-04 13:24:52 -0600

    added testing for french statements files

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,8 +1,15 @@
 test_that("statement data colnames are correct", {
+  # NOTE: I can't set this up like a proper package, because my deployment
+  # (Posit Connect Cloud) seems to require different structure than
+  # what a real package needs, so the working directory doesn't set properly
+  # in `testthat`
+  setwd("../../") # TODO: There must be a better way...
   print(getwd())
   colnames <- c("tags", "statement", "var_name")
-  data <- load_statements(data_lang = "en")
-  test_colnames <- data |> colnames()
-
-  expect_equal(sort(test_colnames), sort(colnames))
+  data_en <- readRDS("data/statements_en.rds")
+  data_fr <- readRDS("data/statements_fr.rds")
+  test_colnames_en <- data_en |> colnames()
+  test_colnames_fr <- data_fr |> colnames()
+  expect_equal(sort(test_colnames_en), sort(colnames))
+  expect_equal(sort(test_colnames_fr), sort(colnames))
 })
