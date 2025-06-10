86b51525a97b5effe8193d7fb2cdbfa7f3bccf87
Reed Merrill <reedjmerrill@gmail.com>
2025-06-10 08:46:48 -0600

    Update app.R documentation

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 # This file is just an entry point into the app. It technically creates a
 # single file app, while still having almost all of the structure of a
-# multi-file app. Its it still uses `ui.R` and `server.R`, just like in
-# a standard two-file R Shiny app.
+# multi-file app. It still uses `ui.R` and `server.R`, just like in a 
+# standard two-file R Shiny app.
 
 # A single entry point in the app's root directory is a requirement of
 # deployment on Posit Connect Cloud.
